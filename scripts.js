7cfad62390ae22a1db330d296d336c67c83faac0
Hugo <hugosimoescontato@gmail.com>
2023-01-22 19:04:36 -0300

    Dia 4

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 const form = document.querySelector('.form')
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-    run: ['01-01', '01-02', '01-06'],
-    water: ['01-04', '01-05'],
-    food: ['01-01', '01-03'],   
+const button = document.querySelector('header button') 
+
+// quando clicar vai acionar a função
+button.addEventListener('click', add)
+// quando houver uma "mudança" vai acionar a função
+form.addEventListener("change", save)
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+    if(dayExists){
+        alert('Dia já incluso ❌')
+    } else {
+        nlwSetup.addDay(today)
+        alert('Dia incluido com sucesso ✅')
+    }
 }
+
+function save(){
+    // salvando o conte´údo no storage do navegador
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
